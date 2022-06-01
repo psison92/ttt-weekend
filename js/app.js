a7083504a6d92f3fcd276d459b0b0c6a2310b3e8
psison <sisonpatrick29@yahoo.com>
2022-06-01 12:59:46 -0500

    declare variables, cache element refs

--- js/app.js
+++ js/app.js
@@ -3,16 +3,21 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const squareEls = document.querySelector(".squares")
+const messageEl = document.querySelector("#message")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
 
+function init() {
+    board = [null, null, null, null, null, null, null, null, null]
+    
+}
